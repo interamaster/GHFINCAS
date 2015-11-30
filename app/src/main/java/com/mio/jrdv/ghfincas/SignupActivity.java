30f7fc7bc9bc9ea3672ada58c5f6d99680d9dd57
joseramondelgadoMACPRO <interamaster@gmail.com>
2015-12-01 00:56:34 +0100

    añadido envio auto de email solo en pantalla de signup y sin reelenar datos correctos

--- app/src/main/java/com/mio/jrdv/ghfincas/SignupActivity.java
+++ app/src/main/java/com/mio/jrdv/ghfincas/SignupActivity.java
@@ -7,8 +7,8 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +30,7 @@ public class SignupActivity extends AppCompatActivity {
 
     private static final String [] EmailTO = {"interamaster@gmail.com"};
 
+    private boolean SendEmailOK=false;
 
 
 
@@ -111,6 +112,10 @@ public class SignupActivity extends AppCompatActivity {
 //eperamos 4 segs
 
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////EMAIL MANUAL//////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+/*
         Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
@@ -184,7 +189,7 @@ public class SignupActivity extends AppCompatActivity {
                 }
                 */
 
-
+/*
                 Intent emailIntent = new Intent(Intent.ACTION_SEND);
                 emailIntent.setData(Uri.parse("mailto:"));
                 emailIntent.setType("message/rfc822");
@@ -214,11 +219,263 @@ public class SignupActivity extends AppCompatActivity {
 
             }
         }, 4000);
+            */
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////AUTOMATICO//////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+        //asi da erro Could not send email
+        //android.os.NetworkOnMainThreadException
+        //hay que hacerlo en en ASYNTASK!!!!
+
+        progressDialog.hide();
+
+
+        new SendMail().execute("");
+
+/*
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String telefono = _telefonoText.getText().toString();
+        String comunidad = _nombreComunidad.getText().toString();
+
+
+
+
+        //el imei:
+
+        TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = mngr.getDeviceId();
+
+        Log.i(" imei:", "" + imei);
+
+
+        String last2CharacteresIemi = imei.substring(Math.max(imei.length() - 2, 0));
+
+        String decryptedpassword="ghfincas"+last2CharacteresIemi;
+
+
+
+
+        //los Valores on correctos asi que los guardamos en le SharedPreference!!!
+
+
+        SharedPreferences pref = getSharedPreferences(LoginActivity.PREFS_NAME, Context.MODE_PRIVATE);
+
+        // We need an editor object to make changes
+        SharedPreferences.Editor edit = pref.edit();
+
+        // Set/Store data
+        edit.putString(LoginActivity.PREF_NOMBREVECINO, name);
+        edit.putString(LoginActivity.PREF_EMAIL, email);
+        edit.putString(LoginActivity.PREF_NOMBRECMUNIDAD, comunidad);
+        edit.putString(LoginActivity.PREF_TELEFONO, telefono);
+        edit.putString(LoginActivity.PREF_PASSWORD, decryptedpassword);
+
+        edit.putBoolean(LoginActivity.PREF_BOOL_LOGINYAOK,false);
+
+
+
+        // Commit the changes
+        edit.commit();
+
+         progressDialog.hide();
+        //en vez de asi..automatico
+
+        Mail m = new Mail("jrdvsoftyopozi@gmail.com", "sevilla4");
+        String[] toArr = {"jrdvsoftyopozi@gmail.com"};
+        m.setTo(toArr);
+        m.setFrom("jrdvsoftyopozi@gmail.com");
+        m.setSubject("Email en pantalla de incidencia!!");
+        m.setBody("Email body 2");
+        try {
+            // m.addAttachment("/sdcard/bday.jpg");
+            if(m.send()) {
+                Toast.makeText(this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
+            } else {
+                //Toast.makeText(this, "Email was not sent.", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Lo siento su movil no esta preparado para mandar emails de manera autoamtica vams ahcerlo de manera manual" +
+                        "", Toast.LENGTH_SHORT).show();
+
+                ManualEmailSiFallaAutomatico();
+
+            }
+        } catch(Exception e) {
+            Log.e("MailApp", "Could not send email", e);
+        }
+
+*/
 
     }
 
 
+    public void SendEmailInBackgroundMio(){
+
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String telefono = _telefonoText.getText().toString();
+        String comunidad = _nombreComunidad.getText().toString();
+
+
+
+
+        //el imei:
+
+        TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = mngr.getDeviceId();
+
+        Log.i(" imei:", "" + imei);
+
+
+        String last2CharacteresIemi = imei.substring(Math.max(imei.length() - 2, 0));
+
+        String decryptedpassword="ghfincas"+last2CharacteresIemi;
+
+
+
+
+        //los Valores on correctos asi que los guardamos en le SharedPreference!!!
+
+
+        SharedPreferences pref = getSharedPreferences(LoginActivity.PREFS_NAME, Context.MODE_PRIVATE);
+
+        // We need an editor object to make changes
+        SharedPreferences.Editor edit = pref.edit();
+
+        // Set/Store data
+        edit.putString(LoginActivity.PREF_NOMBREVECINO, name);
+        edit.putString(LoginActivity.PREF_EMAIL, email);
+        edit.putString(LoginActivity.PREF_NOMBRECMUNIDAD, comunidad);
+        edit.putString(LoginActivity.PREF_TELEFONO, telefono);
+        edit.putString(LoginActivity.PREF_PASSWORD, decryptedpassword);
+
+        edit.putBoolean(LoginActivity.PREF_BOOL_LOGINYAOK, false);
+
+
+
+        // Commit the changes
+        edit.commit();
+
+
+        //en vez de asi..automatico
+
+        Mail m = new Mail("jrdvsoftyopozi@gmail.com", "sevilla4");
+        String[] toArr = {"jrdvsoftyopozi@gmail.com"};
+        m.setTo(toArr);
+        m.setFrom("jrdvsoftyopozi@gmail.com");
+        m.setSubject("Email en pantalla de incidencia!!");
+        m.setBody("Email body 2");
+        try {
+            // m.addAttachment("/sdcard/bday.jpg");
+            if(m.send()) {
+                //Toast.makeText(this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
+                //al hacerlo en backgrousd nos e pueden poner Toast!!!
+                //asin que pongo la ivar a true y ya en el postexecute del asyntask que lo ponga!!!
+
+                SendEmailOK=true;
+
+            } else {
+
+                //al hacerlo en backgrousd nos e pueden poner Toast!!!
+                //asin que pongo la ivar a false  y ya en el postexecute del asyntask que lo ponga!!!
+
+                SendEmailOK=false;
+
+
+                //Toast.makeText(this, "Email was not sent.", Toast.LENGTH_LONG).show();
+               // Toast.makeText(this, "Lo siento su movil no esta preparado para mandar emails de manera autoamtica vams ahcerlo de manera manual" +
+                     //   "", Toast.LENGTH_SHORT).show();
+
+                ManualEmailSiFallaAutomatico();
+
+            }
+        } catch(Exception e) {
+            Log.e("MailApp", "Could not send email", e);
+        }
+
+
+
+
+
+    }
+
+    public void ManualEmailSiFallaAutomatico(){
+
+
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String telefono = _telefonoText.getText().toString();
+        String comunidad = _nombreComunidad.getText().toString();
+
+
+
+
+        //el imei:
+
+        TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = mngr.getDeviceId();
+
+        Log.i(" imei:", "" + imei);
+
+
+        String last2CharacteresIemi = imei.substring(Math.max(imei.length() - 2, 0));
+
+        String decryptedpassword="ghfincas"+last2CharacteresIemi;
+
+
+
+
+
+        // TODO: Hacer envio email manualñ si gfallo el automatico
+        //aqui habria que mandar email con datos!!!
+
+        Log.d("Send email", "Send email");
+
+
+                  /*
+                //enviar solo por email!!!
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                */
+
+
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("message/rfc822");
+
+
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, EmailTO);
+
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Nueva Solicitud alta GHFINCAS  APK");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Gracias por enviarnos su email estos son sus datos:\n" + name + "\n" + email + "\n" + telefono + "\n" + comunidad + "\n signature:" + "Start ENCRYPTED:" +
+                        "%%ADFSDLIFSDLJKHDLKASHDLKHSLKJDHLSDHLKASDKJSKDJJDKJKLDLSKAJDLKAJSDKJLDJLKSJDLKAJSDLKSJADLKJASLDJASKDKJDLKJERIUFH" +
+                        "KLJHFDKHGJKFHKJHGKJHJKFJKDFHGKJHDFKJGHKJDFHGKJFHJKHJFGKJHFDKJGHKJFDHGJKFHGKJDKHGDFKHGJKFDHGKSHFJGHDFSKGKJFDHGKFH" +
+                        ""+imei+"DSADASKDJKASJDLKAJDLKJSALKDJLKJKDLJSALKDJASLKDJLKASDJLKSDJKLSJDLKASJDLKJSLKJDLKJDDSJLKADJSLDSJAKSDKLDSLJ" +
+                        "DNSDJLKAJJKVJKVSDIOUFISODUOIFSJKLKDLSFJLKSDJFLKDSJFKLJSDLKFJKLDSJFLKSJDFLKJSDLKFJKLDJFLKSDJFKLJSDFKLJSDKLFJSDKLF");
+
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    finish();
+                    Log.d("  email", "Finished sending email");
+                } catch (android.content.ActivityNotFoundException ex) {
+                    //no me deja poner un toast dentro de un runnable!!!!
+
+                    Toast.makeText(this, "Lo siento su movil no esta preparado para mandar emails..", Toast.LENGTH_SHORT).show();
+                }
+
+                finish();
+
+
+    }
  
 
     public void onSignupSuccess() {
@@ -276,4 +533,52 @@ public class SignupActivity extends AppCompatActivity {
 
         return valid;
     }
+
+
+
+
+
+    private class SendMail extends AsyncTask<String, Integer, Void> {
+
+
+        protected void onProgressUpdate() {
+            //called when the background task makes any progress
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            SendEmailInBackgroundMio();
+            return null;
+        }
+
+        protected void onPreExecute() {
+            //called before doInBackground() is started
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            if (SendEmailOK) {
+                //funciono ok
+
+                  Toast.makeText(SignupActivity.this, "Su email se envio correctamente!!", Toast.LENGTH_LONG).show();
+
+                finish();
+
+
+            }
+
+            else {
+
+                //ha fallado
+
+                 Toast.makeText(SignupActivity.this, "Email fallo!!!", Toast.LENGTH_LONG).show();
+        }
+
+
+        }
+    }
+
+
 }
